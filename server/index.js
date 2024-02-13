23f9ef5b1aad3f676750dff2f8369a398d802d12
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2024-02-14 01:55:42 +0530

    feat: Set up server and database connection
    This commit implements the following changes:
    - Created Express server to handle HTTP requests and responses.
    - Established connection to MongoDB database using Mongoose.
    - Configured environment variables for database URI and server port.
    - Implemented error handling for database connection failures.

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+require("dotenv").config({ path: "./config/config.env" });
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+const PORT = process.env.PORT || 3000;
+const URI = process.env.URI;
+const cookieParser = require("cookie-parser");
+const file = require("express-fileupload");
+const bodyParser = require("body-parser");
+const dbConnection = require("./config/dbConnection");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(file());
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+dbConnection(URI);
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Handle unhandled rejections
+process.on("unhandledRejection", (promise, e, reason) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+
+  console.log(`Shutting down server due to unhandledRejection`);
+  console.log(`${e.message}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Handle uncaught exceptions
+process.on("uncaughtException", (e) => {
+  console.log(`Error : ${e.message}`);
+  console.log(`Shutting down server due to uncaughtException`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
